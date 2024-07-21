c9ac6564e07f5c90ff7a434cf11ac06dfceb8f32
UNCROOS <any492266@gmail.com>
2024-07-21 21:50:53 +0900

    ㅑㅜㅑㅅ

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import "fmt"
 //fmt.Printl <= print할 때 사용
 
 func main() {
-	// name := "yohan" ==
+	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	name := "yohan"
 	fmt.Println(name)
 	name = "tlqkf"
+	fmt.Println(name)
 }
