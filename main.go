5e517db504f723782b8cdbfdac1411ceaabd745f
UNCROOS <any492266@gmail.com>
2024-07-22 13:33:28 +0900

    init

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 //fmt.Printl <= print할 때 사용
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func superAdd(number ...int) int {
+	defer fmt.Println("I'm done") //특정 함수를 현재 함수가 종료되기 직전에 실행
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-	repeatMe("yohan", "nico", "mike", "pussto")
+	totalLength, up := superAdd("yohan")
+	fmt.Println(totalLength, up)
 }
