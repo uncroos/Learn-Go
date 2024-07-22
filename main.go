de24f4530f4f3c96f3ecb1ee95e24b3a101959fc
UNCROOS <any492266@gmail.com>
2024-07-22 09:41:05 +0900

    init

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import "strings"
+
 //fmt.Printl <= print할 때 사용
+
+func lebAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
