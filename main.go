4594c0a6932f2e473cb332197dc3989261e1cd24
UNCROOS <any492266@gmail.com>
2024-07-22 13:37:45 +0900

    init

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
+import "fmt"
+
 //fmt.Printl <= print할 때 사용
 
-func superAdd(number ...int) int {
-	for number := numbers{
-		
+func superAdd(numbers ...int) int {
+	for index, number := range numbers {
+		fmt.Println(index, number)
 	}
+	return 1
 }
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-	total := superAdd(1, 2, 3, 4, 5)
+	superAdd(1, 2, 3, 4, 5)
 }
