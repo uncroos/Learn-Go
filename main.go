5628eb1ccc692d30db16c4f3fc7d009f2d4c1a7d
UNCROOS <any492266@gmail.com>
2024-07-22 09:52:25 +0900

    ㅑㅜㅑㅅ

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 //fmt.Printl <= print할 때 사용
-
-func lebAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-	totalLenght, upperName := lebAndUpper("yohan")
-	fmt.Println(totalLenght, upperName)
+
 }
