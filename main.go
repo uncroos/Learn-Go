88fded4d9d2373be2b3b399c151a72956750c482
UNCROOS <any492266@gmail.com>
2024-07-22 09:38:42 +0900

    init

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import "fmt"
-
 //fmt.Printl <= print할 때 사용
-
-func multiply(a int, b int) int {
-	return a * b
-}
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-	fmt.Println(multiply(3, 4))
+
 }
