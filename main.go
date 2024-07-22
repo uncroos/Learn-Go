4e55f90997c2889d001ddcfcbd3d59bd3a87ce7b
UNCROOS <any492266@gmail.com>
2024-07-22 13:33:59 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 //fmt.Printl <= print할 때 사용
 
 func superAdd(number ...int) int {
-	defer fmt.Println("I'm done") //특정 함수를 현재 함수가 종료되기 직전에 실행
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+
 }
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-	totalLength, up := superAdd("yohan")
-	fmt.Println(totalLength, up)
+	total := superAdd(1, 2, 3, 4, 5)
 }
