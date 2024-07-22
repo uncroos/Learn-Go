3a48eaea6bd25a8fc854a40013cf89ee453fd9fd
UNCROOS <any492266@gmail.com>
2024-07-22 09:53:45 +0900

    init

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 //fmt.Printl <= print할 때 사용
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-
+	repeatMe("yohan", "nico", "mike", "pussto")
 }
