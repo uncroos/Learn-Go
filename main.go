a0e2c6e711f03031f33cd000fc9c9ff9b03d9ae3
UNCROOS <any492266@gmail.com>
2024-07-23 09:53:06 +0900

    init

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
+import "fmt"
+
 //fmt.Printl <= print할 때 사용
 
 func canIDrink(age int) bool {
-
+	if age < 18 {
+		return false
+	} else {
+		return true
+	}
 }
 
 func main() {
 	// name := "yohan"과 var name String = "yohan"은 같은 코드이다.
 	//name := "yohan" //go가 type을 알아서 결정
-
+	fmt.Println(canIDrink(18))
 }
