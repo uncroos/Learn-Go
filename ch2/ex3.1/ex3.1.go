c5aa191dcfb91adac654be914cc914b6246ba27f
uncroos <any492266@gmail.com>
2024-10-10 23:59:24 +0900

    ex3.1

---
+++ ch2/ex3.1/ex3.1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var minimumWage int = 10		// 변수 minimumWage 선언 및 초기화
+	var workingHour int = 20		// 변수 workingHour 선언 및 초기화
+
+	//변수 income 선언 및 초기화
+	var income int = minimumWage * workingHour
+	
+	//변수 minimumWage, workingHour, income 출력
+	fmt.Println(minimumWage, workingHour, income)
+}
---
+++ ch2/ex3.1/go.mod
@@ -0,0 +1,3 @@
+module Learn-Go/ch2/ex3.1
+
+go 1.23.1
